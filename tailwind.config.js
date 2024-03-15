36cde61ff3f158335e7058cb8841df9e062d7ed5
Saranya Jyothi <saranyajyothi>
2024-03-15 17:15:19 +0530

    tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Fira Sans', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
+
